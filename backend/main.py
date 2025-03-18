9b3fb4596d0e8424af0cbb03a8c8b3ae856ae5a2
MUHAMMAD AQEEL <aqeelmubarak2003@gmail.com>
2025-03-18 15:21:37 +0500

    mysql database setup

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 CORS(app)
 
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'  
+app.config['MYSQL_PASSWORD'] = 'CpYPhP27FrP9Dxw'  
+app.config['MYSQL_DB'] = 'url_shortener'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
